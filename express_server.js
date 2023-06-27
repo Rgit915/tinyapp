354bf65a9646ce5ef736abee3b5bd64fccd15199
Rora Alem <rora.goitom@gmail.com>
2023-06-27 14:05:38 -0400

    Add a POST route that removes a URL resource: POST /urls/:id/delete

--- express_server.js
+++ express_server.js
@@ -68,6 +68,12 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
+app.post("/urls/:id/delete",(req, res) => {
+  const { id } = req.params;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
