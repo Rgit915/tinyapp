540a9b3cecd90dc9b1f58b865bd76165b1594a7c
Rora Alem <rora.goitom@gmail.com>
2023-07-01 20:08:16 -0400

    Add a GET /login endpoint to respond new login form template

--- express_server.js
+++ express_server.js
@@ -117,6 +117,10 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/login", (req, res) =>{
+  res.render("login");
+});
+
 app.post("/login", (req, res) => {
   const { username } = req.body;
   console.log(req.body); // Log the POST request body to the console
