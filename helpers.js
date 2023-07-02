5a618430880a50bd5c9bb086a3f56703c460e670
Rora Alem <rora.goitom@gmail.com>
2023-07-02 16:21:58 -0400

    Create a Helper functions module

---
+++ helpers.js
@@ -0,0 +1,11 @@
+//Email lookup helper function
+const getUserByEmail = (email, users) => {
+  for (const id in users) {
+    if (users[id].email === email) {
+      return users[id];
+    }
+  }
+  return null;
+};
+
+module.exports = {getUserByEmail};
