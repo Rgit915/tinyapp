2bed911cff477c2949f635cca74be98fa8991a6c
Rora Alem <rora.goitom@gmail.com>
2023-07-03 18:28:08 -0400

    Move users, urlDatabse & all helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,34 @@
+// Example defined users and urlDatabase
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+  myTestID: {
+    id: "myTestID",
+    email: "myTestEmail@myTinyApp.ca",
+    password: "tiny-app",
+  },
+};
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+
 //Email lookup helper function
 const getUserByEmail = (email, users) => {
   for (const id in users) {
@@ -8,4 +39,28 @@ const getUserByEmail = (email, users) => {
   return null;
 };
 
-module.exports = {getUserByEmail};
+
+//function to generate a random short URL ID
+const generateRandomString = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgjiklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+};
+
+//function that filters the URLs in the urlDatabase based on logged userID
+const urlsForUser = (id) => {
+  const filteredUrls = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      filteredUrls[url] = urlDatabase[url];
+    }
+  }
+  return filteredUrls;
+};
+
+//Exports
+module.exports = { users, urlDatabase, getUserByEmail, generateRandomString, urlsForUser };
